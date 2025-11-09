ef217244a7d7d9914a0fc6c9afe4f0b03a6f7885
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-11-09 11:56:38 +0100

    Refactor comments and examples in R script

--- other_stuff/apply-Familie + aggregate.r
+++ other_stuff/apply-Familie + aggregate.r
@@ -10,16 +10,16 @@
 
 # - Bsp.: 
 	m <- matrix(data=1:100, nrow=10)
-#	 Mittelwert von jeder Spalte?
+#	 Mean of each column?
 	 
-#	Umständlicher Weg von mir: 
+#	Complicated way: 
 	for (i in 1: ncol(m)){print(mean(m[,i]))}
 	result <- vector("numeric", 10)
 	for (i in 1: ncol(m)){result[[i]] <- mean(m[,i])}
 	 
-	#besserer Weg mit apply: 
+	#Easier with apply: 
 	apply(m, 2, function(x) mean(x))
-	#noch einfacher: 
+	#Even easier: 
 	apply(m, 2, mean)
 
 set.seed(1)
@@ -32,7 +32,7 @@ dimnames(x)[[1]] <- letters[1:8]
 
 #Bsp: 
 l <- list(a = 1:10, b = 11:20) #"the mean of the values in each element"
-#Umständlicher Weg von mir: 
+#Complicated way: 
 mean(as.numeric(l[[1]])) #und
 mean(as.numeric(l[[2]]))
 #besserer Weg mit lapply: 
@@ -44,22 +44,22 @@ sapply(l, mean)
 (x <- mapply(rep, 1:4, 4:1))
 gdata::is.what(x) #is.list, is.recursive u.a.
 #2*x #funktioniert nicht, "Error in x * 2 : non-numeric argument to binary operator"
-lapply(x, function(x){2*x}) #funktioniert! Identisch: 
+lapply(x, function(x){2*x}) #Identical: 
 mapply(function(x){2*x}, x)
 
 #rapply:
 rapply(x, print)
-rapply(x, sum) #identisch: mapply(sum, x)
-rapply(x, length) #identisch: mapply(length, x) #oder auch: unlist(lapply(x, length)) #oder: sapply(x, length)
+rapply(x, sum) #Identical: mapply(sum, x)
+rapply(x, length) #identisch: mapply(length, x) #Or: unlist(lapply(x, length)) #oder: sapply(x, length)
 rapply(x, function(x){2*x})
 
-#sinnvolleres Beispiel (s. http://theautomatic.net/2018/11/13/those-other-apply-functions/):
-lapply(iris, mean) #NA bei nicht-numerischer Variable + Warnmeldung (funktioniert aber auch)
-sapply(iris[sapply(iris, is.numeric)], mean) #wie ich es lösen würde ohne rapply
-rapply(iris, mean, class = "numeric") #elegantere rapply-Lösung
+#More useful examples (s. http://theautomatic.net/2018/11/13/those-other-apply-functions/):
+lapply(iris, mean) #NA bei nicht-numerischer Variable + warning (works nonetheless)
+sapply(iris[sapply(iris, is.numeric)], mean) #how to solve it without rapply
+rapply(iris, mean, class = "numeric") #elegant rapply solution
 
 
-#- by() ist im Prinzip ähnlich wie tapply():
+#- by() is a wrapper for tapply():
 #http://rfunction.com/archives/2330
 
 library(openintro)
@@ -70,20 +70,20 @@ by(textbooks$diff, textbooks$more, summary)
 #entspricht:
 tapply(textbooks$diff, textbooks$more, summary)
 
-#- aggregate-Funktion:
+#- aggregate function:
 #aggregate(x, by, fun), input DF, output DF. Bsp.:
 aggregate(mpg ~ cyl, data=mtcars, FUN=summary)
-#äquivalent, aber weniger schön:
+#Same, but not as nice:
 aggregate(mtcars$mpg ~ mtcars$cyl, FUN=summary)
-#und noch eine dritte mögliche Schreibweise:
+#And a third way of achieving the same:
 aggregate(mtcars$mpg, by=list(mtcars$cyl), FUN=summary)
-#und alternativ eine Variante mit tapply:
+#Alternatively a variant with tapply:
 tapply(mtcars$mpg, mtcars$cyl, summary)
-#und noch eine Variante mit split und lapply:
+#Another one with split and lapply:
 (splitted <- split(mtcars$mpg, mtcars$cyl))
 lapply(splitted, summary)
-#aggregate hat den Vorzug, einen data.frame auszugeben und keine Liste wie tapply. Außerdem kann man damit wohl auch komplexere Sachen machen. Bsp.: Get the mean MPG for Transmission grouped by Cylinder:
-aggregate(mpg~am+cyl, data=mtcars, mean)
+#aggregate has the advantage of returning a data.frame and not a list like tapply. And one can do more complex things (stratified analyses) slightly easier than with tapply. Example: Get the mean MPG for Transmission grouped by Cylinder:
+aggregate(mpg ~ am + cyl, data=mtcars, mean)
 
 #Summarize and stratify by two variables:
 tapply(mtcars$mpg, list(mtcars$am, mtcars$cyl), FUN=mean)
@@ -99,7 +99,7 @@ lapply(data, reitsma)
 summary_reitsma <- function(x, ...){summary(reitsma(x, ...))}
 (x <- lapply(data, summary_reitsma)) #oder: for (i in 1:2) {x[[i]] <- summary_reitsma(data[i])} #oder: for (i in 1:2) {x[[i]] <- summary(reitsma(data[i]))}
 
-#numerische Variablen herausfiltern:
+#Filter the numeric variables:
 dat <- read.csv2("D:\\Daten Samsung Notebook\\Statistik\\R üben\\R Kurs LMU\\TeachingRatings.csv", header=T, sep=",")
 #1.
 nums <- sapply(dat, is.numeric)
@@ -109,7 +109,8 @@ dat[,sapply(dat,is.numeric)]
 #3.
 numerics <- Filter(is.numeric, dat)
 
-#auf diese eine Funktion anwenden:
-sapply(numerics, mean) #oder besser, weil data.frame als Output und ansonsten identisch:
+#Apply a function to them:
+sapply(numerics, mean) #Or slightly better because it returns a data.frame as output (otherwise identical):
 Kmisc::dapply(numerics, mean)
 as.data.frame(sapply(numerics, mean))
+
