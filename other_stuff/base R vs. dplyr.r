5eba91c7900ed856c78b8580835ca8bf37b54651
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-11-09 13:09:25 +0100

    Refactor R script to use dplyr consistently

--- other_stuff/base R vs. dplyr.r
+++ other_stuff/base R vs. dplyr.r
@@ -1,47 +1,47 @@
 
 library(dplyr)
-#Bsp. von http://www.onthelambda.com/2014/02/10/how-dplyr-replaced-my-most-common-r-idioms/
+#Example from http://www.onthelambda.com/2014/02/10/how-dplyr-replaced-my-most-common-r-idioms/
 
-#Datensatz einlesen
+#Load the data set:
 setwd("C:\\Users\\Tobi\\Desktop\\R üben")
 dat <- read.csv2("CrimeStatebyState.csv", header=T, sep=",")
 
-#Struktur/Faktorausprägungen:
-str(dat) #oder um wirklich alle anzuzeigen:
+#Structure/possible values:
+str(dat) #Or to show really all possible values of all variables:
 
 sapply(dat, levels) #base R
 dat %>% sapply(levels) #dplyr
 
-#Verbrechen in New York 2005:
+#Crimes in New York 2005:
 ny_2005 <- dat[dat$Year==2005 & dat$State=="New York",] #base R
 
-attach(dat) #base R, eleganter
+attach(dat) #base R, more elegant
 ny_2005_2 <- dat[Year==2005 & State=="New York",]
 identical(ny_2005, ny_2005_2) #TRUE
 detach(dat)
 
-ny_2005_3 <- subset(dat, Year==2005 & State=="New York") #base R, noch eleganter
+ny_2005_3 <- subset(dat, Year==2005 & State=="New York") #base R, even more elegant
 
-ny_2005_4 <- filter(dat, Year==2005, State=="New York") #dyplr. Kein Vorteil ggü. subset erkennbar.
+ny_2005_4 <- filter(dat, Year==2005, State=="New York") #dyplr. No real difference.
 
-#Spalten auswählen:
+#Select columns:
 ny_2005_type_count <- ny_2005[,c("Type.of.Crime","Count")] #base R
 library(Hmisc)
 ny_2005_type_count_2 <- ny_2005[Cs(Type.of.Crime, Count)] #Hmisc
 ny_2005_type_count_3 <- select(ny_2005, Type.of.Crime, Count) #dplyr
 
-#Neue Variablen erstellen aus existierenden:
+#Create new variables based on existing ones:
 ny_2005$Prop <- ny_2005$Count / sum(ny_2005$Count) #base R
 ny_2005_4 <- mutate(ny_2005_4, Prop=Count/sum(Count)) #dplyr
 
-#Variablen umbenennen:
+#Rename variables:
 #base R:
 names(ny_2005_base)[names(ny_2005_base)=="Type.of.Crime"] <- "Type_of_Crime"
 
 #dplyr:
 rename(ny_2005_dplyr, Type_of_Crime=Type.of.Crime)
 
-#Daten zusammenfassen:
+#Aggregate data:
 summary1 <- aggregate(Count ~ Type.of.Crime, 
 						data=ny_2005, 
 						FUN=sum)
@@ -51,9 +51,9 @@ summary2 <- aggregate(Count ~ Type.of.Crime,
 summary.crime.ny.2005 <- merge(summary1, summary2, by="Type.of.Crime") #base R
 
 by.type <- group_by(ny_2005_4, Type.of.Crime)
-summary.crime.ny.2005 <- summarise(by.type, num.types=n(), counts=sum(Count)) #dplyr. Das soll das Bsp. sein, wo dplyr "wirklich glänzt" im Vgl. zu base R.
+summary.crime.ny.2005 <- summarise(by.type, num.types=n(), counts=sum(Count)) #dplyr.
 
-#und alles zusammen:
+#All together:
 
 #base R:
 dat <- read.csv2("CrimeStatebyState.csv", header=T, sep=",")
