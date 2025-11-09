2b7828026da4c2777847cb870fe160124e4b3491
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-11-09 13:05:02 +0100

    Translate comments and examples to English
    Updated comments and code examples to English for clarity.

--- other_stuff/gdata, gtools, Hmisc, Kmisc, psych.r
+++ other_stuff/gdata, gtools, Hmisc, Kmisc, psych.r
@@ -1,49 +1,49 @@
 
-#####################
-# das "gdata"-Paket #
-#####################
+#######################
+# The "gdata" package #
+#######################
 
 library(gdata)
-#nützliche Funktionen:
-is.what() #gibt einem aus, was für is.()-Abfragen TRUE sind, also AIO von is.numeric, is.list, is.vector, is.array etc.
-#df[df == -99] <- NA (base R, um als -99 kodierte missing in NA umzukodieren)
-#df <- unknownToNA(df, -99, warning=FALSE) #Pendant in gdata (eigentlich nicht besser)
-x <- c(1,2,3,5,NA,6,7,1,NA )
-length(x) #ok, aber wenn man nur die Anzahl der Nicht-NA-Werte wissen will?
+#useful functions:
+is.what() #returns which is.() queries are TRUE. Practically an all-in-one function of is.numeric, is.list, is.vector, is.array etc.
+#df[df == -99] <- NA (base R solution to recode -99 to NA)
+#df <- unknownToNA(df, -99, warning=FALSE) #Same in gdata. (Not much better if you ask me.)
+x <- c(1, 2, 3, 5, NA, 6, 7, 1, NA)
+length(x) #OK, but what if you only want to know the number of values which are not NA?
 length(x[which(!is.na(x))]) #base-R
-nobs(x) #gdata-Äquivalent
-n_obs <- function(x){sum(!is.na(x))} #selbstgeschriebenes Pendant
-#cbindX funktioniert wie cbind, kann aber auch Vektoren/Matrizen unterschiedlicher 
-#Länge/Dimensionen zusammenfügen, "Leerstellen" werden mit NA aufgefüllt.
-ls.funs() #returns a character vector giving the names of function objects in the specified environment (gibt also alle selbstdefinierten Funktionen aus)
-ls.funs("package:gdata") #zeigt alle Funktionen im Paket gdata an.
-#mehrere Variablen auf einmal umbenennen:
+nobs(x) #gdata equivalent
+n_obs <- function(x){sum(!is.na(x))} #self-written equivalent
+#cbindX works like cbind, but can also concatenate vectors/matrices of different length. 
+#Missing/"empty" values are filled up with NA.
+ls.funs() #returns a character vector giving the names of (user-defined) function objects in the specified environment
+ls.funs("package:gdata") #Lists all the functions of the gdata package.
+#Rename several variables at once:
 mtcars2 <- rename.vars(mtcars, from=c("mpg","cyl","disp"), to=c("MPG","Cylinders","Dispersion"))
-#Objekte umbenennen:
+#Rename objects:
 (a <- 1:10)
 mv("a", "A")
 a
 A
-#Vektor aus Matrix erstellen:
+#Create a vector from a matrix:
 (m <- matrix(letters[1:10], ncol=5))
 dim(m) <- NULL #base-R
 m
 (m <- matrix(letters[1:10], ncol=5))
-unmatrix(m) #gdata-Äquivalent
+unmatrix(m) #gdata
 
-#Argumente einer Funktion anzeigen lassen:
-args(cat) #base-R. Nachteil: werden mit Komma getrennt nebeneinander augelistet, daher schwer lesbar.
-Args(cat) #Pendant v. gdata, bei dem die Argumente schön untereinander aufgelistet werden und die Ausprägungen daneben.
-Args(cat, sort=T) #sortiert die Argumente alphabetisch
-ll() #zeigt alle Objekte im Workspace an, wie ls(). Unterschied: zeigt sie schön sortiert untereinander an, zusammen mit der Klasse und der Größe in KB.
-matchcols(iris, with="Sepal") #gibt alle Spalten aus, in denen "Sepal" vorkommt.
-matchcols(iris, with="Sepal", without="Length") #gibt alle Spalten aus, in denen "Sepal" vorkommt, aber nicht "Length".
+#Show arguments of a function:
+args(cat) #base-R. Drawback: Arguments are comma-seperated, thus hard to read.
+Args(cat) #Same in gdata, arguments are nicely listed in a column, along with their possible values next to them.
+Args(cat, sort=T) #Arguments sorted alphabetically.
+ll() #Shows all objects in the work space, similar to ls(). Difference: Shows them nicely sorted, along with their class and size in KB.
+matchcols(iris, with="Sepal") #Shows all columns containing "Sepal".
+matchcols(iris, with="Sepal", without="Length") #Shows all columns containing "Sepal" but not "Length".
 
 
 
-######################
-# das "gtools"-Paket #
-######################
+#######################
+# The "gtools" packge #
+#######################
 
 require(gtools) || install.packages("gtools", dependencies=T)
 
@@ -54,24 +54,18 @@ silly <- function(){
  cat("In 10 years you will be ", age+10, " years old!\n")
 }
 
-#geladene Pakete anzeigen lassen:
+#Show loaded packages:
 loadedPackages()
 
 df1 <- data.frame(A=1:10, B=LETTERS[1:10], C=rnorm(10) )
 df2 <- data.frame(A=11:20, D=rnorm(10), E=letters[1:10] )
-smartbind(df1, df2) #entspricht rbind, aber wenn Variablen im anderen DF nicht vorkommen, funktioniert es trotzdem; diese werden mit NA aufgefüllt.
+smartbind(df1, df2) #Similar to rbind. However, it also works if variable do not exist in the other data frame, those are filled up with NA.
 
-#Das Paket kann eigentlich doch nicht so viel Sinnvolles, wie ich dachte.
+########################
+# The 'Kmisc' package: #
+########################
 
-######################
-# das 'Kmisc'-Paket: #
-######################
-
-#dapply() ist wie l/sapply, gibt aber einen data.frame aus. Bsp.:
 dat <- mtcars
-#Mittelwert jeder Variablen:
-lapply(dat, mean) #sehr hässlich
-sapply(dat, mean) #schöner, aber kein data.frame
 library(Kmisc)
 dapply(dat, mean) #sieht genauso aus wie die sapply-Version, ist aber ein data.frame, s. 
 is.data.frame(dapply(dat, mean))
@@ -80,24 +74,24 @@ list2df(lapply(dat, mean)) #auch aus 'Kmisc'
 remove_chars(x)
 remove_digits(x)
 remove_na(x)
-# tapply_ entspricht tapply, ist aber schneller
-# factor_ entspricht factor, ist aber schneller
-# anatomy entspricht str, ist aber schneller
+# tapply_ equals tapply, but is faster
+# factor_ equals factor, but is faster
+# anatomy equals str, but is faster
 
 
 ###########################################
-# das 'Hmisc'-Paket 
-#(das noch einige weitere nützliche Funktionen beinhaltet):
+# The 'Hmisc' package 
+#(which has a lot more useful functions):
 ###########################################
 
 library(Hmisc)
 test2[1:5,c("INC88","TAX88")] #base-R
-test2[1:5,Cs(INC88,TAX88)] #Pendant in Hmisc (man spart sich die Anführungszeichen bei jeder Variablen)
+test2[1:5,Cs(INC88,TAX88)] #Same with Hmisc (where you don't need the quotation marks for each variable)
 describe(test2)
 
-#####################
-# das 'psych'-Paket #
-#####################
+#######################
+# The 'psych' package #
+#######################
 
 library(psych)
 describe(mtcars)
@@ -105,4 +99,5 @@ describeBy(mtcars, mtcars$am)
 headTail(mtcars)
 corPlot(mtcars)
 corr.test(mtcars)
+
 
