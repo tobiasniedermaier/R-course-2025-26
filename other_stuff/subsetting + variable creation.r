48f189bbaac07ea2e7383eb9db414cd5a1505b9f
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-11-09 12:22:46 +0100

    Translate comments to English and enhance clarity
    Updated comments to English and clarified examples for subsetting and variable creation in R.

--- other_stuff/subsetting + variable creation.r
+++ other_stuff/subsetting + variable creation.r
@@ -1,26 +1,26 @@
 
-#die verschiedenen Möglichkeiten, ein Subset zu bilden:
-#eingebauter Datensatz mtcars; Autos mit mind. 6 Zylindern, mehr als 150 PS und weniger als 3.5 Tonnen:
-#1.) extrem umständlich, ehemals die einzige Variante, die ich kannte
+#The various ways of subsetting a data frame:
+#Built-in data set mtcars; Cars with >= 6 cylinders, more than 150 PS and less than 3.5 tons:
+#1.) extremely complicated:
 mtcars[which(mtcars$cyl>=6 & mtcars$hp>150 & mtcars$wt<3.5),]
-#2.) weniger umständliches Subsetten an sich, dafür Schritt vor- und nachher nötig
+#2.) Less complicated, but additional steps before and after:
 attach(mtcars)
 mtcars[which(cyl>=6 & hp>150 & wt<3.5),]
 detach(mtcars)
-#3.) beste Variante, die noch mit base-R machbar ist:
+#3.) Best soltion using Base R:
 subset(mtcars, cyl>=6 & hp>150 & wt<3.5)
-#4.) noch eine Variante mit base-R, die ich aber nicht schön finde:
-with(mtcars, mtcars[cyl>=6 & hp>150 & wt<3.5,]) #Selten verwendet.
-#5.) mit dem data.table-Paket:
+#4.) Another solution using only Base R:
+with(mtcars, mtcars[cyl>=6 & hp>150 & wt<3.5,]) #Rarely used.
+#5.) Using the data.table package:
 library(data.table)
 dat <- as.data.table(mtcars)
-dat[cyl>=6 & hp>150 & wt<3.5] #data.table ist extrem effizient, erfordert aber eine gewisse Umgewöhnung vgl. mit Base R.
-#6.) mit dem dplyr-Paket:
+dat[cyl>=6 & hp>150 & wt<3.5] #data.table is extremely efficient/fast, especially when handling very large data sets.
+#6.) Using the dplyr-Paket:
 library(dplyr)
 filter(mtcars, cyl>=6, hp>150, wt<3.5)
-#7.) mit dem sqldf-Paket:
+#7.) Using the sqldf-Paket:
 library(sqldf)
-sqldf("select * from mtcars where cyl>=6 and hp>150 and wt<3.5") #Nützlich für SQL-Umsteiger.
+sqldf("select * from mtcars where cyl>=6 and hp>150 and wt<3.5") #Useful for people who worked with SQL previously.
 #zu den weiteren Funktionen von sqldf s.  https://jasdumas.github.io/tech-short-papers/sqldf_tutorial.html#wild_card_match_queries
 
 #within ist im Prinzip überflüssig, da genauso gut alles mit with oder transform machbar. Nur die Syntax ist anders und kürzer, falls mehrere Transformationsschritte. S. https://www.r-bloggers.com/friday-function-triple-bill-with-vs-within-vs-transform/
@@ -45,3 +45,4 @@ anorexia[,wtDiff6 := Postwt - Prewt]
 
 #- man kann mehreren Variablen "in einem Rutsch" den gleichen Wert zuweisen:
 (a <- b <- 5) # weist a den Wert von b zu und b wird 5 zugewiesen.
+
