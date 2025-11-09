39241ea640ca89453fa2b6344b19878327531655
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-11-09 13:13:57 +0100

    Refactor data.table examples and comments

--- other_stuff/data.table.r
+++ other_stuff/data.table.r
@@ -1,7 +1,7 @@
 
-########################
-# das data.table-Paket #
-########################
+##########################
+# The data.table package #
+##########################
 
 #https://www.r-bloggers.com/intro-to-the-data-table-package/
 
@@ -12,35 +12,35 @@ library(data.table)
 DT <- data.table(DF)
 class(DT)
 
-#Mittelwerte von mpg:
+#Means of mpg:
 #base R:
 mean(DT$mpg)
 #data.table:
 DT[,mean(mpg)]
 
-#Subsets bilden:
+#Subsetting:
 #base R:
-DF[DF$mpg>20 & DF$hp>100,] #(oder eine der vielen anderen Möglichkeiten)
+DF[DF$mpg>20 & DF$hp>100,] #(or one of the many other ways)
 #data.table:
-DT[DT$mpg>20 & DT$hp>100,] #man kann mit data tables auch wie bisher mit data frames arbeiten
-DT[mpg>20 & hp>100,] #die Wiederholung des Namens des Datensatzes braucht es aber nicht. Ist im Pinzip wie "eingebautes" attach/detach.
+DT[DT$mpg>20 & DT$hp>100,] #You can work with data tables just like you would with data frames.
+DT[mpg>20 & hp>100,] #It is not necessary to repeat the data set name in data.table, unlike base R. Practically like a "built-in" attach/detach.
 
-#Mittelwerte von mpg in am-Gruppen:
+#Means of mpg in am groups:
 #base R:
 tapply(DF$mpg, DF$am, mean)
 aggregate(mpg ~ am, data=DF, mean)
 #data.table:
 DT[,mean(mpg), by=am]
 
-#gruppiert nach am+cyl:
+#Stratified by am+cyl:
 #base R:
 aggregate(mpg ~ am + cyl, data=DF, mean)
 #data.table:
 DT[,mean(mpg), by=.(am,cyl)]
-#identisch, aber "V1" als "avg" gelebeled:
+#Identical, but "V1" labeled as"avg":
 DT[,.(avg=mean(mpg)), by=.(am,cyl)]
 
-#Anzahl der Zeilen im Datensatz:
+#Number of rows in the data set:
 #base R:
 nrow(DF)
 #data.table:
@@ -58,7 +58,7 @@ head(DF[order(DF$mpg, decreasing=T),], 5)
 #data.table:
 head(DT[order(-mpg)],5)
 
-#neue Variable erstellen (Kilowatt aus PS errechnet):
+#Create a new variable (kw computed from horse powers):
 DT[,kw := hp * 0.735499]
 
 # For rows where the wt is &gt; 1.5 tons count the number of cars by
