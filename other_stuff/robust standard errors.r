d840371b592cb870edd8c4c084a2c1cdf31c8d22
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-11-09 12:35:38 +0100

    Update and rename robuste Standardfehler.r to robust standard errors.r

--- other_stuff/robust standard errors.r
+++ other_stuff/robust standard errors.r
@@ -1,9 +1,9 @@
 
 #######################################################################
-# lineare Regression mit robusten Standardfehlern bei Heteroskedastie # #######################################################################
+# linear regression with robust standard errors because of heteroscedasticity
 
 
-#von:
+#from:
 #http://thestatsgeek.com/2014/02/14/the-robust-sandwich-variance-estimator-for-linear-regression-using-r/
 
 require(sandwich) || install.packages("sandwich", dep=T)
@@ -14,24 +14,25 @@ x <- rnorm(n)
 residual_sd <- exp(x)
 y <- 2*x + residual_sd*rnorm(n)
 
-plot(x,y) #heteroskedastische Daten
+plot(x,y) #heteroscedastic data
 
-#"dumm stellen": Heteroskedastie ignorieren und normales lineares Modell fitten:
+#"Play dumb": Ignore heteroscedasticity and fit a normal linear model:
 mod <- lm(y~x)
 summary(mod)
 par(mfrow=c(2,2))
-plot(mod) #im ersten Plot sieht man schön, dass die Daten stark heteroskedastisch sind.
+plot(mod) #In the first plot you can see nicely that the data are heteroscedastic.
 
-summary(mod)["coefficients"] #SE v. x: 0.311, p-Wert: 0.00025
+summary(mod)["coefficients"] #SE of x: 0.311, p value: 0.00025
 
 library(sandwich)
 
-#erstelle robuste Standardfehler:
+#Create robust standard errors:
 vcovHC(mod, type="HC")
 (sandwich_se <- diag(vcovHC(mod, type="HC"))^0.5) #sandwich_se v. x: 0.584
-coef(mod) - 1.96*sandwich_se #unteres KI
-coef(mod) + 1.96*sandwich_se #oberes KI
+coef(mod) - 1.96*sandwich_se #lower 95% CI
+coef(mod) + 1.96*sandwich_se #upper 95% CI
 
 z_stat <- coef(mod)/sandwich_se
 (p_values <- pchisq(z_stat^2, 1, lower.tail=FALSE))
-#p-Wert: 0.043, vgl. mit 0.00025 mit normalen Standardfehlern
+#p value: 0.043, compared to 0.00025 with normal standard errors
+
