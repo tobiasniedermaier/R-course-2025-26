a008b03be180a5d4a48b91bd99e887123c415d3c
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-11-09 12:48:19 +0100

    Refactor lattice.r with updated comments

--- other_stuff/lattice.r
+++ other_stuff/lattice.r
@@ -1,7 +1,7 @@
 
-#######################
-# das "lattice"-Paket #
-#######################
+#########################
+# The "lattice" package #
+#########################
 
 # https://www.stat.ubc.ca/~jenny/STAT545A/block09_xyplotLattice.html
 
@@ -18,70 +18,70 @@ library(lattice)
 xyplot(lifeExp ~ gdpPercap, jDat) #ohne Raster
 xyplot(lifeExp ~ gdpPercap, jDat, grid=T) #mit Raster
 xyplot(lifeExp ~ gdpPercap, jDat, grid=T, scales=list(x=list(log=10, equispaced.log=F)), type=c("p","r"))
-xyplot(lifeExp ~ gdpPercap, jDat, grid=T, scales=list(x=list(log=10, equispaced.log=F)), type=c("p", "smooth"), col.line="darkorange", lwd=3) #mit fetter, "smoother" orangener Linie dazu
+xyplot(lifeExp ~ gdpPercap, jDat, grid=T, scales=list(x=list(log=10, equispaced.log=F)), type=c("p", "smooth"), col.line="darkorange", lwd=3) #with bold, smooth, orange line
 
-#Pendants in base R:
+#Equivalents in base R:
 plot(jDat$gdpPercap, jDat$lifeExp)
 plot(jDat$gdpPercap, jDat$lifeExp, panel.first = grid())
 plot(jDat$gdpPercap, jDat$lifeExp, log="x", panel.first = grid())
-lines(lowess(jDat$gdpPercap, jDat$lifeExp)) #"smoothed" Linie dazu.
+lines(lowess(jDat$gdpPercap, jDat$lifeExp)) #"smoothed" line added
 
 
-#R in Action, S. 257, Listing 11.1:
+#R in Action, p. 257, Listing 11.1:
 attach(mtcars)
 plot(wt, mpg, main="Basic Scatter plot of MPG vs. Weight", xlab="Car Weight (lbs/1000)",ylab="Miles Per Gallon ", pch=19)
 abline(lm(mpg~wt), col="red", lwd=2, lty=1)
 lines(lowess(wt,mpg), col="blue", lwd=2, lty=2) #!
 
-#analog in lattice:
+#In lattice:
 detach(mtcars)
 xyplot(mpg ~ wt, mtcars, type=c("p","smooth","r"), cex=1.2, col="black", col.line="blue", lty=2, lwd=2, pch=20, main="Lattice Scatter plot of MPG vs. Weight", xlab="Car Weight (lbs/1000)",ylab="Miles Per Gallon ")
 
 
-#Scatterplots mit versch. Farben je nach Gruppe:
+#Scatter plots with different colors according to group:
 #base R:
 set.seed(123)
-DF <- data.frame(A=rnorm(10),B=rpois(10,10),C=sample(1:3,10,replace=T))
+DF <- data.frame(A=rnorm(10), B=rpois(10,10), C=sample(1:3, 10, replace=T))
 
 plot(B ~ A, pch=16, col=C, data = DF)
-legend('bottomright',paste('Group',1:3,sep=" "), col=1:3, pch=16)
+legend('bottomright', paste('Group',1:3,sep=" "), col=1:3, pch=16)
 
 #lattice:
 xyplot(B ~ A, DF, group=C, auto.key=T)
 
-#Multi-panel Plot:
+#Multi-panel plot:
 xyplot(lifeExp ~ gdpPercap | continent, jDat,
        grid = TRUE,
        scales = list(x = list(log = 10, equispaced.log = FALSE)))
-#und mit versch. Farben in jedem Plot:
+#And with different colors in each plot:
 xyplot(lifeExp ~ gdpPercap | continent, jDat,
        group = continent,
        grid = TRUE,
        scales = list(x = list(log = 10, equispaced.log = FALSE)))
 
-#analog in ggplot2 zum Vergleich:
+#same in ggplot2 for comparison:
 multiplot <- ggplot(jDat, aes(x=gdpPercap, y=lifeExp)) + geom_point() 
 final_plot <- multiplot + facet_wrap(~continent)
 final_plot
 
-#und auch noch mit base R:
+#and finally with base R:
 par(mfrow=c(2,2))
 strat <- unique(jDat$continent)
 for (i in 1:length(strat)){
-dat <- jDat[jDat$continent==strat[i],]
-plot(dat$gdpPercap, dat$lifeExp, 
-main=strat[i], 
-xlab="gdpPercap",
-ylab="lifeExp")
+ dat <- jDat[jDat$continent==strat[i],]
+ plot(dat$gdpPercap, dat$lifeExp, 
+ main=strat[i], 
+ xlab="gdpPercap",
+ ylab="lifeExp")
 }
 
-#Histogramme:
+#Histograms:
 
 #base R:
 hist(mtcars$mpg, breaks=nrow(mtcars), col=heat.colors(nrow(mtcars)), xlab="mpg", ylab="count", main="Base R histogram")
 
 #ggplot2:
-qplot(mpg, data=mtcars, main="ggplot2 (qplot) histogram") #oder:
+qplot(mpg, data=mtcars, main="ggplot2 (qplot) histogram") #Or:
 ggplot(mtcars, aes(x=mpg)) + geom_histogram(stat="bin") + ggtitle("ggplot histogram") + theme_bw()
 
 #lattice:
@@ -98,3 +98,4 @@ bwplot(cyl~mpg, data=mtcars, main="Car Milage Data", ylab="Number of Cylinders",
 
 #ggplot2:
 (ggboxplot <- ggplot(data=mtcars, aes(x=cyl, y=mpg, group=cyl)) + geom_boxplot() + theme_bw() + scale_x_discrete(name = "Number of cylinders") + scale_y_continuous(name = "Miles per gallon"))
+
