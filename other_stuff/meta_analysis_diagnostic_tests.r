634edeca0ae2ed95d727b5095f7998a9e96e9149
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-12-10 13:44:52 +0100

    Refactor meta-analysis diagnostic tests code
    Updated code for meta-analysis of diagnostic tests, including improved comments and formatting for clarity.

--- other_stuff/meta_analysis_diagnostic_tests.r
+++ other_stuff/meta_analysis_diagnostic_tests.r
@@ -1,5 +1,5 @@
 
-library(mada) #This package is used to perform a bivariate meta-analysis of sensitivity and specificity of diagnostic tests. In other words, sensitivity and specificity are pooled simultaneously to take their negative correlation into account.
+require(mada) #This package is used to perform a bivariate meta-analysis of sensitivity and specificity of diagnostic tests. In other words, sensitivity and specificity are pooled simultaneously to take their negative correlation into account.
 
 data(AuditC) #Loads a sample data set. Note that the data needs to have the colums TP, FN, FP, TN with the case numbers of true-positives, false-negatives, false-positives, true-negatives for every study.
 
@@ -13,15 +13,21 @@ madad(dat, correction=FALSE)
 madad(dat, correction=0.001)
 madad(dat, correction=0.001, method="clopper-pearson")
 crosshair(dat)
-crosshair(dat, correction=0.001, method="clopper-pearson", pch=3, ylim=c(0.2, 1), yaxs="i", xaxs="i", col=1:nrow(dat))
+crosshair(dat, correction=0.001, 
+          method="clopper-pearson", 
+          pch=3, ylim=c(0.2, 1), 
+          yaxs="i", 
+          xaxs="i", 
+          col=seq_len(nrow(dat)) #fail-safe version of 1:nrow(dat).
+         )
 forest(madad(dat, correction=FALSE), main="Forest plot of diagnostic accuracy studies\ncreated with the mada package", snames=LETTERS[1:14])
 
 summary(reitsma(dat)) #reitsma() performs the meta-analysis, summary(reitsma()) provides a more informative output. However, it is a bit inconvenient if the pooled sensitivity and false-positive rate (= 1-specificity) are all you want.
 summary(reitsma(dat))$coefficients #This extracts the coefficients (Se+ 1-Sp) from the output.
 fit_reitsma <- reitsma(dat)
-b <- summary(fit_reitsma)
-c <- b$coefficients
-c #The same "broken down".
+summary_fit  <- summary(fit_reitsma)
+coef_fit     <- summary_fit $coefficients
+coef_fit     #The same "broken down".
 
 plot(fit_reitsma, main="SROC curve (bivariate model) for AUDIT−C data")
 points(fpr(dat), sens(dat), pch=2)
@@ -31,7 +37,7 @@ dev.off()
 
 #Leave out one study at a time and perform the meta-analysis again, using a for-loop:
 for(i in 1:nrow(dat)){
-  print(paste("Leave out study", i))
+  cat("Leave out study", i, "\n")
   print(summary(reitsma(dat[-i,]))$coefficients)
   cat("\n")
 }
@@ -39,7 +45,7 @@ for(i in 1:nrow(dat)){
 #Leave out one study at a time and perform the meta-analysis again, using a while-loop:
 i <- 1
 while(i <= nrow(dat)){
-  print(paste("Leave out study", i))
+  cat("Leave out study", i, "\n")
   print(summary(reitsma(dat[-i,]))$coefficients)
   cat("\n")
   i <- i+1
@@ -47,11 +53,11 @@ while(i <= nrow(dat)){
 
 #Funnel plot to address publication bias:
 require(metafor)
-require(mada)
 require(MVPBT)
 data(cervical) #from the MVPBT package
 
-LAG <- cervical[cervical$method==2,]
+LAG <- cervical[cervical$method==2,] #or:
+#LAG <- subset(cervical, method == 2) #(same)
 
 fit1 <- reitsma(LAG) #from the mada package
 summary(fit1)     # results of the bivariate meta-analysis
@@ -67,5 +73,3 @@ detach(LAG)
 attach(dta1)
 bifunnel(y,S) #same as bifunnel(dta1$y, dta1$S)
 detach(dta1)
-
-
