2c0d1e3386a96c33bce37f704d0fcc7c2136f63f
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-12-10 12:14:50 +0100

    Fix replacement argument in two_dice function

--- other_stuff/two_dice.r
+++ other_stuff/two_dice.r
@@ -1,7 +1,8 @@
 
 two_dice <- function(size=100){
- res1 <- sample(1:6, size=size, replace=T)
- res2 <- sample(1:6, size=size, replace=T)
+ res1 <- sample(1:6, size=size, replace=TRUE)
+ res2 <- sample(1:6, size=size, replace=TRUE)
  res <- res1+res2
  return(res)
 }
+
