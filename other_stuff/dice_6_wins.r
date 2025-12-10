e35d6d6ce56e553e4204e27379b5ad0188a91863
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-12-10 12:12:21 +0100

    Refactor dice_6_wins function with ifelse
    Refactored dice_6_wins function to use ifelse for efficiency.

--- other_stuff/dice_6_wins.r
+++ other_stuff/dice_6_wins.r
@@ -1,10 +1,18 @@
 dice_6_wins <- function(size=100){
- res <- as.character(sample(1:6, size=size, replace=T))
+ res <- as.character(sample(1:6, size=size, replace=TRUE))
  
- for(i in 1:size){
- if (res[i]=="6"){res[i] <- "win"} else {
- res[i] <- "lose"}
-  }
+ for(i in seq_len(size)){
+  if (res[i]=="6"){res[i] <- "win"} else {
+  res[i] <- "lose"}
+ }
  return(res)
+}
+dice_6_wins()
+
+#Or without for-loop:
+dice_6_wins <- function(size = 100) {
+  dice <- sample(1:6, size = size, replace = TRUE)
+  res  <- ifelse(dice == 6, "win", "lose")
+  return(res)
 }
 dice_6_wins()
