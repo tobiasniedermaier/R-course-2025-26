8cc9a2a1ff095b354481b814eeebf325564eb7cc
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-12-11 10:08:47 +0100

    Refactor mpg conversion methods in R script

--- other_stuff/loops_vs_Map_vs_vectorization.r
+++ other_stuff/loops_vs_Map_vs_vectorization.r
@@ -17,7 +17,7 @@ dat_backup <- dat
 #Now we have three columns with measurements in mpg that we want to convert to l/100 km. But we don't want to do that one by one (because, what if we had 100 variables to convert?)
 
 #1st way: using a for-loop:
-for (i in 1:3){
+for (i in seq_len(ncol(dat))){
  dat[,i] <- dat[,i] * 1.6
  dat[,i] <- dat[,i] / 3.7
  dat[,i] <- 1/dat[,i]
@@ -27,7 +27,7 @@ for (i in 1:3){
 #2nd way: using a while-loop:
 dat <- dat_backup
 i <- 1
-while(i<4){
+while(i <= ncol(dat)){
  dat[,i] <- dat[,i] * 1.6
  dat[,i] <- dat[,i] / 3.7
  dat[,i] <- 1/dat[,i]
@@ -38,13 +38,30 @@ while(i<4){
 #3rd way: using a repeat-loop:
 dat <- dat_backup
 i <- 1
-repeat{
- dat[,i] <- dat[,i] * 1.6
- dat[,i] <- dat[,i] / 3.7
- dat[,i] <- 1/dat[,i]
- dat[,i] <- dat[,i] * 100
- i <- i+1
- if (i == 4) break
+repeat {
+  dat[, i] <- dat[, i] * 1.6
+  dat[, i] <- dat[, i] / 3.7
+  dat[, i] <- 1 / dat[, i]
+  dat[, i] <- dat[, i] * 100
+  i <- i + 1
+  if (i > ncol(dat)) break
+}
+
+#Intermediate step for 4th way:
+
+## From other_stuff/mpg_to_lp100km.r:
+mpg_to_lp100km <- function(data=NULL, mpgvar=NULL, plausib_bound=10){
+ if(is.null(data)) stop("No data set provided!")
+ if(!is.numeric(data[,mpgvar])) stop("Variable needs to be numeric!")
+ if(min(data[,mpgvar]) <= plausib_bound){
+ print(data[data[,mpgvar] <= plausib_bound,])
+ warning("At least one observation has an unrealistically high fuel consumption.\nIn addition to liters per 100km, the implausible obervations are returned.\nPlease check your input data and/or exclude those observations..")
+ }
+ kmpg <- data[,mpgvar] * 1.6 #km per gallon
+ tmp1 <- kmpg / 3.7 # km per liter
+ tmp2 <- 1/tmp1 #liter per km
+ res <- tmp2*100 #liter per 100 km
+ return(res)
 }
 
 #4th way: using the higher-order function Map()
@@ -62,3 +79,12 @@ FUN <- function(x){
 }
 vFUN <- Vectorize(FUN)
 vFUN(dat)
+
+#Although here, it would also work without explicit Vectorize():
+mpg_to_lp100km_vec <- function(x) {
+  100 / (x * 1.6 / 3.7)
+}
+
+dat <- dat_backup
+dat <- mpg_to_lp100km_vec(dat)
+
