70ecad64c39d4d3644a465bd1a1934b15f1acbc6
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-12-13 13:33:56 +0100

    Update pROC optimized.r

--- other_stuff/pROC optimized.r
+++ other_stuff/pROC optimized.r
@@ -6,17 +6,16 @@ dat <- aSAH
 women <- subset(dat, dat$gender=="Female")
 men <- subset(dat, dat$gender=="Male")
 
-roc_men_ndka <- roc(men$outcome, men$ndka, auc=T, percent=T)
-plot.roc(roc_men_ndka, print.auc=T, main="Test", xaxs="i", yaxs="i")
+roc_men_ndka <- roc(men$outcome, men$ndka, auc=TRUE, percent=TRUE)
+plot.roc(roc_men_ndka, print.auc=TRUE, main="Test", xaxs="i", yaxs="i")
 
-# Now we generalize it in a function so that we don't need to repeat everything if we want to do the same for women:
+# und das will ich jetzt verallgemeinern, sodass ich es nicht wiederholen muss, wenn ich dasselbe für Frauen mache
 
 pROC <- function(data,marker){
  temp <- roc(data$outcome, data[,marker], auc=T, percent=T)
  plot.roc(temp, print.auc=T, main=paste("ROC Curve of", marker), xaxs="i", yaxs="i")
 }
 
-roc_men_ndka <- pROC(men,"ndka")
-roc_women_ndka <- pROC(women,"ndka")
-roc_men_s100b <- pROC(men,"s100b")
-
+roc_men_ndka <- pROC(men, "ndka")
+roc_women_ndka <- pROC(women, "ndka")
+roc_men_s100b <- pROC(men, "s100b")
