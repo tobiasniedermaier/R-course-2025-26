902b440c65fbb64a6a6afef1d54cfdce6c359e73
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2025-12-13 13:35:32 +0100

    Update pROC optimized.r

--- other_stuff/pROC optimized.r
+++ other_stuff/pROC optimized.r
@@ -9,13 +9,14 @@ men <- subset(dat, dat$gender=="Male")
 roc_men_ndka <- roc(men$outcome, men$ndka, auc=TRUE, percent=TRUE)
 plot.roc(roc_men_ndka, print.auc=TRUE, main="Test", xaxs="i", yaxs="i")
 
-# und das will ich jetzt verallgemeinern, sodass ich es nicht wiederholen muss, wenn ich dasselbe für Frauen mache
+# Generalized, so that we don't need to repeat all steps when doing the same for women:
 
-pROC <- function(data,marker){
- temp <- roc(data$outcome, data[,marker], auc=T, percent=T)
+pROC <- function(data, marker){
+ temp <- roc(data$outcome, data[,marker], auc=TRUE, percent=TRUE)
  plot.roc(temp, print.auc=T, main=paste("ROC Curve of", marker), xaxs="i", yaxs="i")
 }
 
 roc_men_ndka <- pROC(men, "ndka")
 roc_women_ndka <- pROC(women, "ndka")
 roc_men_s100b <- pROC(men, "s100b")
+
