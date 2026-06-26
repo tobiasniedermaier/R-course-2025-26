769e641405b0abe841752a095696e639dbe184ea
tobiasniedermaier <161356441+tobiasniedermaier@users.noreply.github.com>
2026-06-26 14:29:08 +0200

    Restore common statistical tests in R

--- other_stuff/statistical_tests.r
+++ other_stuff/statistical_tests.r
@@ -1,11 +1,11 @@
 
-#Common statistical tests:
+# Common statistical tests:
 
 # Author: Tobias Niedermaier
 
-#see also: https://stats.idre.ucla.edu/r/whatstat/what-statistical-analysis-should-i-usestatistical-analyses-using-r/
+# see also: https://stats.idre.ucla.edu/r/whatstat/what-statistical-analysis-should-i-usestatistical-analyses-using-r/
 
-##Chi-squared:
+## Chi-squared:
 library(vcd)
 (mytable <- xtabs(~ Treatment + Improved, data = Arthritis))
 #         Improved
@@ -16,20 +16,20 @@ library(vcd)
 chisq.test(mytable)
 chisq.test(mytable, correct=FALSE)
 
-##Fisher's exact test:
+## Fisher's exact test:
 fisher.test(mytable)
 fisher.test(mytable, simulate.p.value=TRUE, B=5000)
 
-##LR, Pearson, Phi, contingency coeff., Cramer's V:
+## LR, Pearson, Phi, contingency coeff., Cramer's V:
 assocstats(mytable) #from the vcd package (visualizing categorical data)
 summary(assocstats(mytable)) #slightly more comprehensive
 
-##One-sample t test: (example taken from http://r-statistics.co/Statistical-Tests-in-R.html)
+## One-sample t test: (example taken from http://r-statistics.co/Statistical-Tests-in-R.html)
 set.seed(100)
 x <- rnorm(50, mean = 10, sd = 0.5)
 t.test(x, mu=10)
 
-##Two-sample t-Test (Welch test):
+## Two-sample t-Test (Welch test):
 t.test(1:10, y = c(7:20))
 
 #Note that you can also use the "formula syntax" and that you can specify if you want to test one- or two-sided and the alpha level:
@@ -49,7 +49,7 @@ b = c(12.7, 13.6, 12.0, 15.2, 16.8, 20.0, 12.0, 15.9, 16.0, 11.1)
 
 t.test(a,b, paired=TRUE)
 
-#A practical example for the paired t-test:
+# A practical example for the paired t-test:
 # BMI before and after a diet for 10 people
 bmi_before <- c(30.1, 28.4, 26.7, 32.0, 29.5, 27.8, 31.2, 33.0, 25.9, 29.0)
 bmi_after  <- c(28.9, 27.5, 25.9, 30.1, 28.8, 27.0, 30.3, 31.4, 25.1, 28.4)
@@ -63,13 +63,13 @@ diet_bmi <- data.frame(
 diet_bmi
 
 t.test(diet_bmi$bmi_before, diet_bmi$bmi_after, paired = TRUE)
-#On average, BMI decreased by about 1.02 units (because before − after is positive).
+# On average, BMI decreased by about 1.02 units (because before − after is positive).
 
-#For comparison the non-paired t-test:
+# For comparison the non-paired t-test:
 t.test(diet_bmi$bmi_before, diet_bmi$bmi_after, paired = FALSE)
-#Same mean difference (as one would expect), but much higher p value because it "throws away" the information on the individual BMI change and only uses the mean differences across all participants instead.
+# Same mean difference (as one would expect), but much higher p value because it "throws away" the information on the individual BMI change and only uses the mean differences across all participants instead.
 
-#z test:
+# z test:
 library(BSDA)
 x <- c(7.8, 6.6, 6.5, 7.4, 7.3, 7., 6.4, 7.1, 6.7, 7.6, 6.8)
 y <- c(4.5, 5.4, 6.1, 6.1, 5.4, 5., 4.1, 5.5)
@@ -81,15 +81,15 @@ t.test(x, y) #For comparison.
  # The alternative hypothesis is that this difference is not 2.
  # A confidence interval for the true difference will be computed.
 
-#F-Test for equality of variances:
+# F-Test for equality of variances:
 var.test(a, b) #Tests: H0: σ1² = σ2² vs H1: σ1² ≠ σ2²
 
-#Another example:
+# Another example:
 mpg_4 <- mtcars$mpg[mtcars$cyl == 4]
 mpg_6 <- mtcars$mpg[mtcars$cyl == 6]
 var.test(mpg_4, mpg_6)
 
-#F test in linear regression to check if the model is better than the null model:
+# F test in linear regression to check if the model is better than the null model:
 mylm <- lm(mpg ~ wt + hp + cyl, data=mtcars)
 summary(mylm) #F-statistic: 50.17 on 3 and 28 DF,  p-value: 2.184e-11 -> Yes, the model is significantly better than the null model. Tests if all coefficients (for wt, hp, cyl) are 0 (H0) vs. if at least one of them is non-zero.
 #Alternatively:
@@ -97,7 +97,7 @@ lm1 <- lm(mpg ~ wt + hp + cyl, data=mtcars)
 lm2 <- lm(mpg ~ 1, data=mtcars) #intercept only
 anova(lm1, lm2) #same p value
 
-#ANOVA:
+# ANOVA:
 set.seed(42)
 group_A <- rnorm(30, mean = 5,  sd = 1)
 group_B <- rnorm(30, mean = 6,  sd = 1.5)
@@ -114,7 +114,7 @@ TukeyHSD(anova_model)
 # Kruskal-Wallis Test
 kruskal.test(value ~ group, data = mydf)
 
-#McNemar-Test:
+# McNemar-Test:
 m <- matrix(c(30, 10,
               5,  40),
             nrow = 2,
@@ -122,14 +122,13 @@ m <- matrix(c(30, 10,
 
 rownames(m) <- c("before_Pos", "before_Neg")
 colnames(m) <- c("after_Pos", "after_Neg")
-
 m
 
 # McNemar test
 mcnemar.test(m)
 
 
-#Chi-squared test:
+# Chi-squared test:
 (mymat <- matrix(round(runif(16, min=0, max=15)), nrow=4, byrow=TRUE))
 rownames(mymat) <- LETTERS[1:4]
 colnames(mymat) <- letters[1:4]
@@ -137,7 +136,7 @@ chisq.test(mymat)
 chisq.test(mymat, correct=FALSE)
 fisher.test(mymat, simulate.p.value=TRUE, B=5000)
 
-#Wilcoxon rank sum test (Mann-Whitney-U test):
+# Wilcoxon rank sum test (Mann-Whitney-U test):
 numeric_vector <- c(20, 29, 24, 19, 20, 22, 28, 23, 19, 19)
 wilcox.test(numeric_vector, mu=20, conf.int = TRUE)
 
@@ -147,7 +146,7 @@ wilcox.test(x, y, alternative = c("two.sided"))
 wilcox.test(x, y, alternative = c("less"))
 wilcox.test(x, y, alternative = c("greater"))
 
-#Another example:
+# Another example:
 wilcox.test(mpg ~ am, data=mtcars)
 wilcox.test(mpg ~ am, data=mtcars, correct=FALSE)
 wilcox.test(mpg ~ am, data=mtcars, conf.int=TRUE)
@@ -157,20 +156,20 @@ wilcox.test(mpg ~ am, data=tmp)
 wilcox.test(mpg ~ am, data=tmp, correct=FALSE)
 wilcox.test(mpg ~ am, data=tmp, conf.int=TRUE)
 
-#Wilcoxon signed-rank test (for PAIRED samples!):
+# Wilcoxon signed-rank test (for PAIRED samples!):
 wilcox.test(diet_bmi$bmi_before, diet_bmi$bmi_after, paired = TRUE) #Note how much smaller the p value is if you use the information that you have paired (before-after) data. Just like above for the t-test.
 
-#Correlation test:
+# Correlation test:
 cor.test(mtcars$mpg, mtcars$hp)
 
-#non-parametric (rank-)correlation test:
+# non-parametric (rank-)correlation test:
 cor.test(mtcars$mpg, mtcars$hp, method = "spearman")
 
-#Shapiro-Test for normal distribution:
+# Shapiro-Test for normal distribution:
 normaly_disb <- rnorm(100, mean=5, sd=1)
 shapiro.test(normaly_disb) #If p<0.05 -> not normally distributed. On the other hand, a p-value >=0.05 does NOT confirm the presence of a normal distribution! A non-significant test statistic might also mean that your sample was simly too small.
 
-#Binomial test:
+# Binomial test:
 successes <- 5
 trials <- 12
 binom.test(successes, trials) #Tests if p is significantly different from 0.5 by default).
@@ -179,11 +178,11 @@ binom.test(successes, trials, alternative="greater")
 binom.test(successes, trials, alternative="less")
 binom.test(successes, trials, conf.level=0.90)
 
-#Median test:
+# Median test:
 require(coin) || (install.packages("coin") && (require(coin)))
 median_test(mpg ~ factor(am), data = mtcars) #Note that the grouping variable needs to be a factor.
 
-#Convenience function for installing (if not installed yet) and loading a package:
+# Convenience function for installing (if not installed yet) and loading a package:
 get_package <- function(pkg) {
   if (!require(pkg, character.only = TRUE)) {
     install.packages(pkg)
@@ -194,13 +193,13 @@ get_package <- function(pkg) {
 get_package("RVAideMemoire")
 mood.medtest(mpg ~ factor(am), data = mtcars)
 
-#Kolmogorov-Smirnov test: (Used to check whether 2 samples follow the same distribution)
+# Kolmogorov-Smirnov test: (Used to check whether 2 samples follow the same distribution)
 x <- rnorm(50)
 y <- runif(30)
 # Do x and y come from the same distribution?
 ks.test(x, y)
 
-#Levene test (for equality of variances):
+# Levene test (for equality of variances):
 get_package("car")
 
 set.seed(123)
@@ -217,12 +216,12 @@ df <- data.frame(group, value)
 leveneTest(value ~ group, data = df)
 
 
-#Further statistical tests are available in the package "lawstat".
+# Further statistical tests are available in the package "lawstat".
 
-#Required case number for a t-test (difference in means) at a pre-defined statistical power and significance and mean difference (effect size):
+# Required case number for a t-test (difference in means) at a pre-defined statistical power and significance and mean difference (effect size):
 pwr.t.test(d=0.8, sig.level=0.05, power=0.9, type="two.sample", alternative="two.sided") #where d=(mean group 1 - mean group 2) / sqrt(pooled sampling variance)
 
-#Required case number for a test of differences in proportions:
+# Required case number for a test of differences in proportions:
 p1 <- 0.5 #z.B. Se. v.gFOBT f. CRC
 p2 <- 0.7 #z.B. Se. v.FIT f. CRC
 pwr.2p.test(h=ES.h(p1,p2), sig.level=0.05, power=0.8, alternative="two.sided")
